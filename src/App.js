18efda64852212f25742d68249e789228760f9c8
kunlefash <kunlefashmayop@gmail.com>
2022-09-09 14:23:06 -0300

    Add react-router to project

--- src/App.js
+++ src/App.js
@@ -1,69 +1,14 @@
-import { useState, useEffect } from 'react';
-import { ethers } from 'ethers';
+import React from 'react';
+import { Routes, Route} from "react-router-dom";
+import Home from './components/home/Home';
 
 function App() {
-  const [haveMetamask, sethaveMetamask] = useState(true);
-  const [accountAddress, setAccountAddress] = useState('');
-  const [isConnected, setIsConnected] = useState(false);
-
-  const { ethereum } = window;
-
-  useEffect(() => {
-    const { ethereum } = window;
-    const checkMetamaskAvailability = async () => {
-      if (!ethereum) {
-        sethaveMetamask(false);
-      }
-      sethaveMetamask(true);
-    };
-    checkMetamaskAvailability();
-  }, []);
-
-  const connectWallet = async () => {
-    try {
-      if (!ethereum) {
-        sethaveMetamask(false);
-      }
-      const accounts = await ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      setAccountAddress(accounts[0]);
-      setIsConnected(true);
-    } catch (error) {
-      setIsConnected(false);
-    }
-  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {haveMetamask ? (
-          <div className="App-header">
-            {isConnected ? (
-              <div className="card">
-                <div className="card-row">
-                  <h3>Wallet Address:</h3>
-                  <p>
-                    {accountAddress.slice(0, 4)}...
-                    {accountAddress.slice(38, 42)}
-                  </p>
-                </div>
-              </div>
-            ) : (
-              <img src="" className="App-logo" alt="logo" />
-            )}
-            {isConnected ? (
-              <p className="info">🎉 Connected Successfully</p>
-            ) : (
-              <button className="btn" onClick={connectWallet}>
-                Connect
-              </button>
-            )}
-          </div>
-        ) : (
-          <p>Please Install MataMask</p>
-        )}
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+      </Routes>
     </div>
   );
 }
